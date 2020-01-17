830fe677026203091316ca6a2a341f452b9782f6
Ali Abrar <aliabrar@gmail.com>
2020-01-17 17:03:49 -0500

    v0.2.0.0: Make interface more like System.FSNotify

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,5 +1,10 @@
 # Revision history for reflex-fsnotify
 
+## 0.2.0.0
+
+* Deprecate `watchDirectory`
+* Add `watchDir` and `watchTree` corresponding to the functions in `System.FSNotify`
+
 ## 0.1.0.0
 
 * Initial release containing `watchDirectory`.
--- src/Reflex/FSNotify.hs
+++ src/Reflex/FSNotify.hs
@@ -4,7 +4,11 @@ Description: Watch for filesystem changes in reflex
 -}
 
 {-# LANGUAGE FlexibleContexts #-}
-module Reflex.FSNotify where
+module Reflex.FSNotify
+  ( watchDirectory
+  , watchDir
+  , watchTree
+  ) where
 
 import Control.Concurrent
 import Control.Monad
@@ -12,16 +16,42 @@ import Control.Monad.IO.Class
 import Reflex
 import qualified System.FSNotify as FS
 
+-- A type synonym to disambiguate Reflex 'Event's from 'System.FSNotify.Event'
+type FSEvent = FS.Event
+
 -- | Watch a directory for changes
+{-# DEPRECATED watchDirectory "Use `watchDir cfg path (const True)` instead" #-}
 watchDirectory
   :: (Reflex t, TriggerEvent t m, PerformEvent t m, MonadIO (Performable m))
   => FS.WatchConfig
   -> Event t FilePath
   -> m (Event t FS.Event)
-watchDirectory cfg path =
+watchDirectory cfg path = watchDir cfg path (const True)
+
+wrapWatch
+  :: (Reflex t, TriggerEvent t m, PerformEvent t m, MonadIO (Performable m))
+  => (FS.WatchManager -> FilePath -> FS.Action -> IO a)
+  -> FS.WatchConfig
+  -> Event t FilePath
+  -> m (Event t FSEvent)
+wrapWatch f cfg path =
   performEventAsync $ ffor path $ \p cb -> liftIO $ void $ forkIO $
     FS.withManagerConf cfg $ \mgr -> do
-      _ <- FS.watchTree mgr p (const True) cb
+      _ <- f mgr p cb -- FS.watchTree mgr p (const True) cb
       forever $ threadDelay 1000000
 
+watchDir
+  :: (Reflex t, TriggerEvent t m, PerformEvent t m, MonadIO (Performable m))
+  => FS.WatchConfig
+  -> Event t FilePath
+  -> FS.ActionPredicate
+  -> m (Event t FSEvent)
+watchDir cfg path evFilter = wrapWatch (\mgr p -> FS.watchDir mgr p evFilter) cfg path
 
+watchTree
+  :: (Reflex t, TriggerEvent t m, PerformEvent t m, MonadIO (Performable m))
+  => FS.WatchConfig
+  -> Event t FilePath
+  -> FS.ActionPredicate
+  -> m (Event t FSEvent)
+watchTree cfg path evFilter = wrapWatch (\mgr p -> FS.watchTree mgr p evFilter) cfg path
